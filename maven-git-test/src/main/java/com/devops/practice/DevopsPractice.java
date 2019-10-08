1feea4a77519bb6b9980481356e59893cfc578e6
megha-cloud <56022420+megha-cloud@users.noreply.github.com>
2019-10-08 16:23:20 +0530

    First commit for test cases

---
+++ maven-git-test/src/main/java/com/devops/practice/DevopsPractice.java
@@ -0,0 +1,11 @@
+package com.devops.practice;
+
+public class DevopsPractice {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Calculations for test program are complete");
+
+	}
+}
